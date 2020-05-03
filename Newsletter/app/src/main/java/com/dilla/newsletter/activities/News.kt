9b5e80d23158a8e92cd57b02facb1ef38edb5f04
xalamander13 <hanifahperdilla@gmail.com>
2020-05-03 10:44:25 +0700

    delete two project

--- Newsletter/app/src/main/java/com/dilla/newsletter/activities/News.kt
+++
@@ -1,8 +0,0 @@
-package com.dilla.newsletter.activities
-
-class News(title: String, body: String, date: String) {
-    var id: Int = 0
-    val title: String = title
-    val body: String = body
-    val date: String = date
-}
